0f21d3e67a1bf3d7550cccaa01269b71270eb7ee
RANGKI <140485155+RANGKI@users.noreply.github.com>
2025-05-11 20:38:52 +0700

    Create Route for Data Personal Page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\DataPersonalController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(DataPersonalController::class)->group(function() {
+    Route::get('/admin/data_personal','view');
 });
