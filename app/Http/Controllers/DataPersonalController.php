f70efd475fa02c476f1ced4c42462faa8553902b
RANGKI <140485155+RANGKI@users.noreply.github.com>
2025-05-11 20:45:31 +0700

    DataPersonalController setup

--- app/Http/Controllers/DataPersonalController.php
+++ app/Http/Controllers/DataPersonalController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DataPersonal;
 use Illuminate\Http\Request;
 
 class DataPersonalController extends Controller
 {
-    //
+    public function show() {
+        $users = DataPersonal::all();
+        return view('admin_dashboard.data_personals.data_personals',['users' => $users]);
+    }
 }
