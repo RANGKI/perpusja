167fc9b2488ef85bf5625a8d11c7cb29cc5239b5
RANGKI <140485155+RANGKI@users.noreply.github.com>
2025-05-12 19:02:21 +0700

    Create logout route

--- routes/web.php
+++ routes/web.php
@@ -36,4 +36,5 @@ Route::controller(DataAdminController::class)->group(function() {
 Route::controller(LoginController::class)->group(function() {
     Route::get('/login','show');
     Route::post('/login','login');
+    Route::post('/logout','logout');
 });
