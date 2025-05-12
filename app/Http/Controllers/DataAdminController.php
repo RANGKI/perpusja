e12857aef90865d9b7b64d4018577c3a0e2c34d0
RANGKI <140485155+RANGKI@users.noreply.github.com>
2025-05-12 16:11:33 +0700

    Setup DataAdmin controller

--- app/Http/Controllers/DataAdminController.php
+++ app/Http/Controllers/DataAdminController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DataAdmin;
 use Illuminate\Http\Request;
 
 class DataAdminController extends Controller
 {
-    //
+    public function show() {
+        $admins = DataAdmin::all();
+        return view('admin_dashboard.data_admins.data_admin',['admins' => $admins]);
+    }
 }
