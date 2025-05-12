213b16976eb263f8b75a32d56f178d961146261c
RANGKI <140485155+RANGKI@users.noreply.github.com>
2025-05-12 16:06:49 +0700

    Setup DataAdmin model

--- app/Models/DataAdmin.php
+++ app/Models/DataAdmin.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class DataAdmin extends Model
 {
-    //
+    protected $table = 'data_admin';
+
+    protected $primaryKey = 'id'; 
+
+    protected $fillable = [
+        'image_path',
+        'username',
+        'email',
+        'password'
+    ];
 }
