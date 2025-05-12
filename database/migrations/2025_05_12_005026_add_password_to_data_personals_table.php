1fd162a8ec9b1fa40b635de8fcc1da17497ae8d2
RANGKI <140485155+RANGKI@users.noreply.github.com>
2025-05-12 07:53:11 +0700

    add password coloumn logic

--- database/migrations/2025_05_12_005026_add_password_to_data_personals_table.php
+++ database/migrations/2025_05_12_005026_add_password_to_data_personals_table.php
@@ -6,23 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('data_personals', function (Blueprint $table) {
-            //
+            $table->string('password')->after('phone_number');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('data_personals', function (Blueprint $table) {
-            //
+            $table->dropColumn('password');
         });
     }
 };
