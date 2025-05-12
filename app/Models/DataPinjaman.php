deb0f8c5dc1edf7b207e90042d6d94ed788a45a4
RANGKI <140485155+RANGKI@users.noreply.github.com>
2025-05-12 14:40:57 +0700

    Setup DataPinjaman Model

--- app/Models/DataPinjaman.php
+++ app/Models/DataPinjaman.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class DataPinjaman extends Model
 {
-    //
+    protected $table = 'data_pinjaman';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'user_id',
+        'book_id',
+        'tanggal_pinjam',
+        'tanggal_kembali',
+        'status'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(DataPersonal::class, 'user_id');
+    }
+
+    public function book()
+    {
+        return $this->belongsTo(DataStock::class, 'book_id');
+    }
 }
